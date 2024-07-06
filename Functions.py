426b1985b198fdd6a076b76b00b2580a5f50eb60
ffonchk <youia2555@gmail.com>
2024-07-06 14:06:52 +0700

    update README

--- Functions.py
+++
@@ -1,8 +0,0 @@
-def add_numbers(a, b):
-    """ฟังก์ชันสำหรับบวกเลข a และ b"""
-    sum = a + b
-    return sum
-
-# เรียกใช้งานฟังก์ชัน
-result = add_numbers(5, 3)
-print("ผลรวม:", result)  # ผลลัพธ์คือ 8
